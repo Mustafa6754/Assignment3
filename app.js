eb60f3a46ae13e19a88e04c20da3e82b872483e4
Mustafa6754 <144300820+Mustafa6754@users.noreply.github.com>
2023-09-14 18:44:07 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+let studentNames = [];
+studentNames.push("John");
+studentNames.push("Alice");
+studentNames.push("Bob");
+
+// Declare and initialize a string array
+var myStrings = ["Hello", "World", "This", "Is", "An", "Array"];
+
+let numbers = [1, 2, 3, 4, 5];
+
+// Declare and initialize a boolean array
+const booleanArray = [true, false, true, false];
+
+let mixedArray = [1, "Hello", 3.14, true, [1, 2, 3]];
+
+// Define an array to store student scores
+let studentScores = [89, 76, 92, 78, 85];
+
+// Sort the array in ascending order using the sort method
+studentScores.sort(function(a, b) {
+  return a - b;
+});
+
+// Print the sorted scores
+console.log("Sorted Student Scores (Ascending Order):");
+console.log(studentScores);
+
+// Initialize an array with city names
+const cities = ["New York", "Los Angeles", "Chicago", "Houston", "Miami", "San Francisco", "Boston", "Seattle"];
+
+var arr = ["This", "is", "my", "cat"];
+
+// Create an empty FIFO queue
+const fifoQueue = [];
+
+// Function to add a value to the end of the queue
+function enqueue(value) {
+  fifoQueue.push(value);
+}
+
+// Create an empty array to serve as the stack
+const stack = [];
+
+// Push values into the stack
+stack.push(1);
+stack.push(2);
+stack.push(3);
+
+// Access and remove values in reverse order (LIFO)
+while (stack.length > 0) {
+  const value = stack.pop();
+  console.log(value);
+}
+document.write (Apple, Samsung, Motorola, Nokia, Sony & Haier)
+
+
